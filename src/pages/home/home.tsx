68250d4792683440bb4179e19187365a52ac4303
Shaghayegh Ghavami <ghavami.shgh@gmail.com>
2023-02-22 17:24:46 +0330

    Update home page

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,6 +1,29 @@
+import Layout from 'components/layout/layout'
+import { Content, Box, Context, H1, Shake, Text , ImageWrapper} from './home.style'
+import Image from 'components/image/image';
+const Logo = require('images/shaghayegh.png');
+
 function Home() {
-    return <h1>Start create your website!</h1>;
-  }
-  
-  export default Home;
-  
+  const job = '<Frontend developer />'
+  return (
+    <Layout home={true}>
+      <Content>
+        <Box>
+          <ImageWrapper>
+            <Image alt="shaghayegh" src={Logo} />
+          </ImageWrapper>
+        </Box>
+        <Context>
+          <H1>
+            Hi There
+            <Shake>👋</Shake>
+          </H1>
+          <Text>I'm Shaghayegh Ghavami</Text>
+          <Text>{job}</Text>
+        </Context>
+      </Content>
+    </Layout>
+  )
+}
+
+export default Home
