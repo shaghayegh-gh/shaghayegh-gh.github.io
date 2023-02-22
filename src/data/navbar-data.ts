b16d94d1e4bf0ef01ea6b698d1b32a4b95008906
Shaghayegh Ghavami <ghavami.shgh@gmail.com>
2023-02-22 17:03:46 +0330

    Add data

---
+++ src/data/navbar-data.ts
@@ -0,0 +1,12 @@
+type Items = { name: string; path: string }[]
+
+export const Items: Items = [
+  {
+    name: 'Home',
+    path: '/',
+  },
+  {
+    name: 'About me',
+    path: '/about',
+  },
+]
