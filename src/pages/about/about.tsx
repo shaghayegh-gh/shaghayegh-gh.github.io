3e0e3300a6a36d98aabc8df9794f177c8ca37d90
Shaghayegh Ghavami <ghavami.shgh@gmail.com>
2023-02-22 17:48:00 +0330

    Add skills component

--- src/pages/about/about.tsx
+++ src/pages/about/about.tsx
@@ -1,14 +1,15 @@
 import Layout from 'components/layout/layout'
 import AboutMe from 'components/about-me/about-me'
 import Experiences from 'components/experiences/experience'
-
-import { me,experiences } from 'data/about/about-data'
+import Skills from 'components/skills/skills'
+import { me, experiences, skills } from 'data/about/about-data'
 
 const About = () => {
   return (
     <Layout>
       <AboutMe me={me} />
       <Experiences experiences={experiences} />
+      <Skills skills={skills} />
     </Layout>
   )
 }
